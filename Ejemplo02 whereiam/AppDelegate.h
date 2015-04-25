61c0bc277bea40953c99544d20208a34e8698681
hansellesteban <hansell.ramos@gmail.com>
2015-04-25 13:10:54 -0500

    Initial Commit

---
+++ Ejemplo02 whereiam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ejemplo02 whereiam
+//
+//  Created by Hansel Ramos Osorio on 4/25/15.
+//  Copyright (c) 2015 Hansel Ramos Osorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ejemplo02 whereiam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ejemplo02 whereiam
+//
+//  Created by Hansel Ramos Osorio on 4/25/15.
+//  Copyright (c) 2015 Hansel Ramos Osorio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
